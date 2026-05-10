3cc04abaa255c4a42daaa870ab0db7568a68e5d4
xin627720-dotcom <xin627720@gmail.com>
2026-05-10 09:44:08 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/flashcard_screen.dart';
+import 'core/services/storage_service.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  final storageService = StorageService();
+  await storageService.init();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => storageService,
+      child: const VocabFlowApp(),
+    ),
+  );
+}
+
+class VocabFlowApp extends StatelessWidget {
+  const VocabFlowApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '词汇流 - FREE高考英语',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: const FlashcardScreen(),
+    );
+  }
+}
